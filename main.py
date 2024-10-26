38c650084225b944c1c3553dc4c5ea1fb518589c
MuriloFreitasSpinelli <murilodefreitasspinelli@gmail.com>
2024-10-27 00:12:53 +0200

    more features

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ current_tasks = {}
 completed_tasks = {}
 task_by_id = []
 
-usr = ""
+usr_input = ""
 
 current_id = 0
 
@@ -23,36 +23,61 @@ def add_task():
     task_id = make_id()
     current_tasks[task_id] = {"Titel": title, "Description": description, "Project": project}
 
+def sort_ids():
+    pass
 
 def remove_task():
-    to_go = int(input("Task ID:"))
-    key = task_by_id[to_go]
+    to_del = int(input("Task ID:"))
+    key = task_by_id[to_del]
     current_tasks.pop(key)
     print(current_tasks)
 
+def reload():
+    load_tasks()
+
+def clear_all():
+    global current_tasks
+    global completed_tasks
+    global task_by_id
+    current_tasks = {}
+    completed_tasks = {}
+    task_by_id = []
 
 def load_tasks():
-    pass
+    global current_tasks
+    global completed_tasks
+    global task_by_id
+
+    with open('tasks.pkl', 'rb') as f:
+        current_tasks = pickle.load(f)
+    #with open('completed_tasks.pkl', 'rb') as g:
+    #    completed_tasks = pickle.load(g)
 
 def save_tasks():
-    pass
+    with open('tasks.pkl', 'wb') as f:
+        pickle.dump(current_tasks, f)
 
-while usr != "4":
+while usr_input != "4":
     print("Hello, welcome to the debug, To-Do")
-    print("1: Add Task\n2: Remove Task\n3: Show Tasks\n4: Exit")
-    usr = input()
+    print("1: Add Task\n2: Remove Task\n3: Show Tasks\n4: Exit\n5: Custom command\n6:Save\n7:Load")
+    usr_input = input()
 
-    if usr == "1":
+    if usr_input == "1":
         add_task()
 
-
-    elif usr == "2":
+    elif usr_input == "2":
         remove_task()
 
-    elif usr == "3":
+    elif usr_input == "3":
         task_by_id = list(current_tasks)
         print(task_by_id)
         print(current_tasks)
-
-
+    elif usr_input == "5":
+        usr_input = eval(input("Command:"))
+        usr_input()
+        print("Command completed")
+    elif usr_input == "6":
+        save_tasks()
+    elif usr_input == "7":
+        load_tasks()
 
